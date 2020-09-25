f427ca3caeff0120ff0d33bb7b9888b1a3eeddbc
DImka <MashinaDonnu>
2020-09-25 12:46:05 +0300

    add build folder

---
+++ task/build/precache-manifest.dcd467c0fd0f176af622cc5a3e02be0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d075c2baa6cdde17fda40497f29ae9e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd6e334b47cd7f66dbd8",
+    "url": "/static/css/main.61211471.chunk.css"
+  },
+  {
+    "revision": "7261b734d35496ab69f8",
+    "url": "/static/js/2.b9e3b7c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9e3b7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd6e334b47cd7f66dbd8",
+    "url": "/static/js/main.1d53c425.chunk.js"
+  },
+  {
+    "revision": "afd9fccc04eb1a18eb43",
+    "url": "/static/js/runtime-main.0c7a2f62.js"
+  },
+  {
+    "revision": "c1b2293586edb83bcd635cdb4e299109",
+    "url": "/static/media/Logo original RGB.c1b22935.jpg"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/static/media/Untitled-1Artboard1copy.37a75b54.jpg"
+  }
+]);
